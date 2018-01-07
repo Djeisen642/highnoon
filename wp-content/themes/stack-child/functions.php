69e7fc03d4e1e24eb067529a27b54bde83343ab1
Jason Suttles <Jason Suttles>
2018-01-07 13:27:24 -0500

    add stack-child

--- wp-content/themes/stack-child/functions.php
+++ wp-content/themes/stack-child/functions.php
@@ -1,14 +1,15 @@
 <?php
-function stack_child_enqueue_styles() {
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
 
-    $parent_style = 'stack-style';
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'stack-child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
-}
-add_action( 'wp_enqueue_scripts', 'stack_child_enqueue_styles' );
-?>
+if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
+    function chld_thm_cfg_parent_css() {
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'bootstrap','ebor-icons','ebor-plugins' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 110 );
+
+// END ENQUEUE PARENT ACTION
